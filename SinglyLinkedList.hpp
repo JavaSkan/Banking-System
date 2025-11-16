8f3a629e6150ae5662875fcb1c47ff3d8f69485b
Aymen <aymen.abdelmoumen11@gmail.com>
2025-11-16 02:58:41 +0100

    branch for headers

---
+++ SinglyLinkedList.hpp
@@ -0,0 +1,17 @@
+//SinglyLinkedList.h
+#ifndef SINGLYLINKEDLIST_H
+#define SINGLYLINKEDLIST_H
+#define TEMPLATE template <typename T>
+
+TEMPLATE
+struct Node {
+    T data;
+    Node<T>* next;
+};
+
+TEMPLATE
+struct List {
+Node<T>* head;
+int size;
+};
+#endif // SINGLYLINKEDLIST_H
