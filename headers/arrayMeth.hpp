2e5ec07b483d009208cf17ac2eebadef2ecb78f7
mohamedazizayouni <aziz.ayouni.mc@gmail.com>
2025-11-16 13:47:37 +0100

    tweaked some errors

--- headers/arrayMeth.hpp
+++ headers/arrayMeth.hpp
@@ -5,7 +5,7 @@
 
 TEMPLATE array<T> createArray();
 TEMPLATE void destroyArray(array<T>* p);
-TEMPLATE bool isEmpty(const array<T>& p)
+TEMPLATE bool isEmpty(const array<T>& p);
 TEMPLATE bool isFull(const array<T>& p);
 
 
