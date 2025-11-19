a04c2efcc395c05350cf608e3322723fc2ab9471
Aymen <aymen.abdelmoumen11@gmail.com>
2025-11-19 23:19:40 +0100

    GUI

--- compile.bat
+++ compile.bat
@@ -1,1 +1,2 @@
-g++ .\source\*.cpp -o main
+g++ -g src\*.cpp libs\imgui\*.cpp libs\imgui\backends\*.cpp -Iheaders -Ilibs\SDL2/include -Ilibs\imgui -Llibs/SDL2/lib -lSDL2main -lSDL2 -o BankingSystem.exe 
+pause
