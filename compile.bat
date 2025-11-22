40d86956114c1f522750d433e91e68460bc17d44
MelikBibi <melikbibi02@gmail.com>
2025-11-22 16:27:04 +0100

    minor

--- compile.bat
+++ compile.bat
@@ -1,3 +1,3 @@
-g++ -g src/*.cpp -o bankingSystem.exe
+g++ -g -std=c++17 src\main.cpp src\CustomerInterface.cpp src\EmployeeInterface.cpp src\SinglyLinkedListMeth.cpp src\DoublyLinkedListMeth.cpp src\DataStructsFunctions.cpp src\Dates.cpp src\Loans.cpp -Iinclude -Isdk/include -lole32 -luuid -lversion -lshlwapi -mwindows -o BankingSystem.exe
 pause
  
