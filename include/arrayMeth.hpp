a126147827b705cac623c865c4a6c35589c096d2
MelikBibi <melikbibi02@gmail.com>
2025-11-22 16:07:12 +0100

    Merge branch 'Headers-and-interfaces' of https://github.com/JavaSkan/Banking-System into Headers-and-interfaces

--- include/arrayMeth.hpp
+++ include/arrayMeth.hpp
@@ -7,47 +7,18 @@ using namespace std;
 #define TEMPLATE template <typename T>
 
 // --- Definitions ---
-TEMPLATE
-Array<T> createArray() {
-    return Array<T>{nullptr, 0};
-}
+TEMPLATE Array<T> createArray(int capacity);
+TEMPLATE void destroyArray(Array<T>* p);
 
-TEMPLATE
-void destroyArray(Array<T>* p) {
-    delete[] p->data;
-    p->size = 0;
-}
+TEMPLATE bool isEmpty(const Array<T>& p);
+TEMPLATE bool isFull(const Array<T>& p);
+TEMPLATE int arraySize(const Array<T>& p);
 
-TEMPLATE
-bool isEmpty(const Array<T>& p) {
-    return p.size == 0;
-}
+TEMPLATE void displayArray(const Array<T>& p);
+TEMPLATE int addElement(Array<T>& p, T e, int pos);
+TEMPLATE Array<T> copyArray(const Array<T>& p);
 
-TEMPLATE
-bool isFull(const Array<T>& p) {
-    return p.data != nullptr && p.size > 0; // example, adjust logic as needed
-}
 
-TEMPLATE
-int arraySize(const Array<T>& p) {
-    return p.size;
-}
-
-TEMPLATE
-void displayArray(const Array<T>& p) {
-    for (int i = 0; i < p.size; ++i)
-        cout << p.data[i] << " ";
-    cout << endl;
-}
-
-TEMPLATE
-Array<T> copyArray(const Array<T>& p) {
-    Array<T> c;
-    c.size = p.size;
-    c.data = new T[c.size];
-    for (int i = 0; i < c.size; ++i)
-        c.data[i] = p.data[i];
-    return c;
-}
+#include "arrayMeth.tpp"
 
 #endif
