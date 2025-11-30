eae28e44b8a2a78a117d073cdb4dae0042f1982f
Aymen <aymen.abdelmoumen11@gmail.com>
2025-11-30 18:56:04 +0100

    .

--- GUI/CustomerScript.js
+++ GUI/CustomerScript.js
@@ -2,15 +2,49 @@ let LoanCount=2;
 
 function table() {
     let table = document.getElementById("LoansTable");
-    table.classList.add("LoansTab");
     if (!table) return;
+
+    table.classList.add("LoansTab");
+
     for (let i = 0; i < LoanCount; i++) { 
         let row = document.createElement("tr");
         let cell = document.createElement("td");
-        let cellId = `(Loan${i})`;
-        cell.id = cellId;
-        cell.style.backgroundColor="rgb(58, 58, 60)";
+        cell.id = `Loan${i}`;
+
+        let innerTable = document.createElement("table");
+
+        getLoansLine().then((LoanString) => {
+
+            // Parse the incoming string
+            const list = LoanString.split("*");   // list[0]..list[9]
+            const celltext=[
+                "Loan ID=",
+                "Loan Type=",
+                "Loan Status=",
+                "Loan Primary Amount=",
+                "Loan Interest Rate=",
+                "Loan Amount paid=",
+                "Loan Remaining Balance=",
+                "Loan Start Date=",
+                "Loan End DateID=",
+            ]
+            // [Id,type,lstatus,pr_amnt,ir,ap,rb,sd,ed]
+
+            for (let j = 0; j < 9; j++) {
+                let innerRow = document.createElement("tr");
+                let innerCell = document.createElement("td");
+
+                innerCell.id = `Loan${i}_${j}`
+                innerCell.innerText = celltext+list[j];  // safe fallback
+
+                innerRow.appendChild(innerCell);
+                innerTable.appendChild(innerRow);
+            }
+        });
+
+        cell.append(innerTable);
         row.appendChild(cell);
         table.appendChild(row);
     }
 }
+
